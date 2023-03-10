2132b1c3dbd2ce7e65e5a5a969849d2d8ab7d6d6
snapdragon teaks <97859152+Elchanz3@users.noreply.github.com>
2023-03-10 15:11:53 -0300

    Update make-menuconfig-clang.sh

--- make-menuconfig-clang.sh
+++ make-menuconfig-clang.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 
-*/put your clang patch here*/
+#             */put your clang patch here*/
+make /home/chanz22/clang-r437112/bin/clang exynos9820-d2s_defconfig
 make /home/chanz22/clang-r437112/bin/clang menuconfig
